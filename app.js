73f7046e8c3cfb1e3c3dad75c60696528c3e31f3
Rohitmeerwal <rohitkumar302013@gmail.com>
2024-05-14 13:04:15 +0530

    changes in middleware

--- app.js
+++ app.js
@@ -2,32 +2,31 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 dotenv.config();
-import cors from "cors"
+import cors from "cors";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
-import userRouter from "./routes/user.routes.js"
+import userRouter from "./routes/user.routes.js";
 import connectDB from "./config/connectDB.js";
 import { app, server } from "./socket/socket.js";
-
 
 // const app = express();
 const port = process.env.PORT || 8000;
 
 const DATABASEURL = process.env.MONGODB_URL;
 connectDB(DATABASEURL);
-app.use(cors(
-  {
-    origin:"https://chat-app-i08f.onrender.com",
+app.use(
+  cors({
+    origin: "https://chat-app-i08f.onrender.com",
     // origin:"http://localhost:3001",
-    preflightContinue:true,
-    credentials:true
-  }
-))
+    preflightContinue: true,
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/users", authRoutes);
 app.use("/api/messages", messageRoutes);
-app.use("/api/user", userRouter)
+app.use("/api/user", userRouter);
 
 server.listen(port, () => {
   console.log(`server listening on ${port}`);
--- utils/generateToken.js
+++ utils/generateToken.js
@@ -4,10 +4,13 @@ const generateTokenandCookie = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET_KEY, {
     expiresIn: "15d",
   });
+  const isProduction = process.env.NODE_ENV === 'production';
+
   res.cookie("jwt", token, {
-    maxAge: 15 * 24 * 60 * 60 * 1000,
+    maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
     httpOnly: true,
-    sameSite: "strict",
+    sameSite: isProduction ? "strict" : "lax", 
+    secure: isProduction, 
   });
 };
 export default generateTokenandCookie;
